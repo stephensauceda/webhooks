48832e92482e3665c4678f4979cf6dba73ae025b
Stephen Sauceda <stephen.sauceda@gmail.com>
2024-10-31 09:18:51 -0400

    send-webmentions (#3)

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 import { syndicate } from './lib/syndicate/index.js'
 import { validateWebhook } from './lib/validateWebhook.js'
+import { send as sendWebmentions } from './lib/webmentions/index.js'
 
 export const webhooks = async (req, res) => {
   const authorizedWebhook = validateWebhook(req)
-
-  if (!authorizedWebhook) {
-    res.status(401).send('Unauthorized')
-    return
-  }
 
   if (req.method !== 'POST') {
     res.status(405).send('Method not allowed')
     return
   }
 
+  if (!authorizedWebhook) {
+    res.status(401).send('Unauthorized')
+    return
+  }
+
   const { path } = req
-  const { post } = req.body
+  // Ghost sends pretty much the same data for all webhooks,
+  // but the top-level key changes depending on the content type
+  const payload = Object.values(req.body)[0]
 
   try {
     switch (path) {
       case '/syndicate':
-        await syndicate(post)
+        await syndicate(payload)
+        break
+      case '/send-webmentions':
+        await sendWebmentions(payload)
         break
     }
   } catch (error) {
---
+++ src/lib/webmentions/index.js
@@ -0,0 +1,34 @@
+export function send(post) {
+  const WEBMENTIONS_ENDPOINT = 'https://webmention.app/check'
+
+  if (!post?.current?.url) {
+    return Promise.resolve('No URL to send webmentions to')
+  }
+
+  const query = new URLSearchParams({
+    url: post.current.url,
+    token: process.env.WEBMENTION_APP_TOKEN,
+  }).toString()
+
+  return fetch(`${WEBMENTIONS_ENDPOINT}?${query}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(async response => {
+    if (!response.ok) {
+      return Promise.reject(`Failed to send webmentions: ${response.status}`)
+    }
+
+    const { urls: sentMentions } = await response.json()
+    const message = `Sent ${sentMentions.length} webmentions for ${post.current.url}`
+
+    console.log(message)
+
+    sentMentions.forEach(({ target, status }) => {
+      console.log(`${status}: ${target}`)
+    })
+
+    return message
+  })
+}
