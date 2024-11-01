bef28e4fc2a5ba8e627b9ee2921776c78989af2f
Stephen Sauceda <stephen.sauceda@gmail.com>
2024-10-31 21:05:11 -0400

    organize things with describe blocks

--- src/lib/syndicate/index.test.js
+++ src/lib/syndicate/index.test.js
@@ -12,55 +12,57 @@ afterEach(() => {
   vi.resetAllMocks()
 })
 
-test('logs when a service is not implemented', () => {
-  expect.assertions(1)
-  const post = {
-    current: {
-      slug: '/no-service',
-      tags: [{ name: '#syndicate:foo' }],
-    },
-  }
+describe('syndicate', () => {
+  test('logs when a service is not implemented', () => {
+    expect.assertions(1)
+    const post = {
+      current: {
+        slug: '/no-service',
+        tags: [{ name: '#syndicate:foo' }],
+      },
+    }
 
-  return syndicate(post).then(() => {
-    expect(error).toHaveBeenCalledWith('Service foo not implemented.')
+    return syndicate(post).then(() => {
+      expect(error).toHaveBeenCalledWith('Service foo not implemented.')
+    })
   })
-})
 
-test('call the service function when the service is implemented', async () => {
-  expect.assertions(1)
+  test('call the service function when the service is implemented', async () => {
+    expect.assertions(1)
 
-  const post = {
-    current: {
-      slug: '/mastodon-success',
-      tags: [{ name: '#syndicate:mastodon' }],
-    },
-  }
+    const post = {
+      current: {
+        slug: '/mastodon-success',
+        tags: [{ name: '#syndicate:mastodon' }],
+      },
+    }
 
-  syndicateToMastodon.mockResolvedValueOnce(
-    `Syndicated ${post.current.slug} to mastodon.`
-  )
+    syndicateToMastodon.mockResolvedValueOnce(
+      `Syndicated ${post.current.slug} to mastodon.`
+    )
 
-  await syndicate(post)
-  expect(log).toHaveBeenCalledWith(
-    `Syndicated ${post.current.slug} to mastodon.`
-  )
-})
+    await syndicate(post)
+    expect(log).toHaveBeenCalledWith(
+      `Syndicated ${post.current.slug} to mastodon.`
+    )
+  })
 
-test('logs when the service call fails', async () => {
-  syndicateToMastodon.mockRejectedValueOnce(
-    'Failed to syndicate to mastodon: something went wrong'
-  )
-  expect.assertions(1)
+  test('logs when the service call fails', async () => {
+    syndicateToMastodon.mockRejectedValueOnce(
+      'Failed to syndicate to mastodon: something went wrong'
+    )
+    expect.assertions(1)
 
-  const post = {
-    current: {
-      slug: '/mastodon-fail',
-      tags: [{ name: '#syndicate:mastodon' }],
-    },
-  }
+    const post = {
+      current: {
+        slug: '/mastodon-fail',
+        tags: [{ name: '#syndicate:mastodon' }],
+      },
+    }
 
-  await syndicate(post)
-  expect(error).toHaveBeenCalledWith(
-    `Failed to syndicate to mastodon: something went wrong`
-  )
+    await syndicate(post)
+    expect(error).toHaveBeenCalledWith(
+      `Failed to syndicate to mastodon: something went wrong`
+    )
+  })
 })
--- src/lib/syndicate/mastodon.test.js
+++ src/lib/syndicate/mastodon.test.js
@@ -16,38 +16,40 @@ afterEach(() => {
   vi.restoreAllMocks()
 })
 
-test('makes a POST request to the MASTODON API', async () => {
-  expect.assertions(1)
-  fetch.mockResolvedValue({ ok: true })
-  await syndicateToMastodon(post)
-  expect(fetch).toHaveBeenCalledWith(MASTODON_API_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer token`,
-    },
-    body: JSON.stringify({
-      status: `${post.current.custom_excerpt} ${post.current.url}`,
-    }),
-  })
-})
-
-test('solves with syndication message when successful', async () => {
-  expect.assertions(1)
-  fetch.mockResolvedValue({ ok: true })
-  await expect(syndicateToMastodon(post)).resolves.toBe(
-    `Syndicated ${post.current.slug} to Mastodon.`
-  )
-})
-
-test('rejects to failed syndication message when unsuccessful', async () => {
-  expect.assertions(1)
-  fetch.mockResolvedValue({
-    ok: false,
-    json: () => Promise.resolve({ error: 'something went wrong' }),
+describe('Mastodon', () => {
+  test('makes a POST request to the MASTODON API', async () => {
+    expect.assertions(1)
+    fetch.mockResolvedValue({ ok: true })
+    await syndicateToMastodon(post)
+    expect(fetch).toHaveBeenCalledWith(MASTODON_API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer token`,
+      },
+      body: JSON.stringify({
+        status: `${post.current.custom_excerpt} ${post.current.url}`,
+      }),
+    })
   })
 
-  await expect(syndicateToMastodon(post)).rejects.toBe(
-    `Failed to syndicate to Mastodon: something went wrong`
-  )
+  test('solves with syndication message when successful', async () => {
+    expect.assertions(1)
+    fetch.mockResolvedValue({ ok: true })
+    await expect(syndicateToMastodon(post)).resolves.toBe(
+      `Syndicated ${post.current.slug} to Mastodon.`
+    )
+  })
+
+  test('rejects to failed syndication message when unsuccessful', async () => {
+    expect.assertions(1)
+    fetch.mockResolvedValue({
+      ok: false,
+      json: () => Promise.resolve({ error: 'something went wrong' }),
+    })
+
+    await expect(syndicateToMastodon(post)).rejects.toBe(
+      `Failed to syndicate to Mastodon: something went wrong`
+    )
+  })
 })
