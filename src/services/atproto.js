68f705498a24c0ab2754c96f49335bdea9e54752
Stephen Sauceda <stephen.sauceda@gmail.com>
2024-11-14 23:00:04 -0500

    let's try this

--- src/services/atproto.js
+++ src/services/atproto.js
@@ -1,19 +1,15 @@
 /* v8 ignore start */
 import { AtpAgent as Agent, RichText as RT } from '@atproto/api'
 
-async function init() {
-  const AtpAgent = new Agent({
-    service: 'https://bsky.social'
-  })
+const agent = new Agent({
+  service: 'https://bsky.social'
+})
 
-  await AtpAgent.login({
-    identifier: 'stephensauceda.com',
-    password: process.env.BLUESKY_APP_PASSWORD
-  })
+await agent.login({
+  identifier: 'stephensauceda.com',
+  password: process.env.BLUESKY_APP_PASSWORD
+})
 
-  return AtpAgent
-}
-
-export const AtpAgent = init()
+export const AtpAgent = agent
 export const RichText = RT
 /* v8 ignore end */
